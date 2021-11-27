53852eba28c4af28500877907a1f44df114f2fc5
Dr. Hax <alexfh2001@gmail.com>
2021-11-27 17:19:57 -0600

    first commit

--- arduinoPolls/views.py
+++ arduinoPolls/views.py
@@ -1,6 +1,14 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Default view
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@csrf_exempt
+def generateUser(request):
+    if request.method == 'POST':
+        return HttpResponse("POST method used")
+    else:
+        return render(request, '1.html')
